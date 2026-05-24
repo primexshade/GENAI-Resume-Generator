b71f4a885b57a60d5b8d8d87fcdc74b6e3daf46d
Aryan Tiwari <taryan54@gmail.com>
2026-05-24 20:49:23 +0530

    Login, Logout, Get-Me APIs created and Middleware completed

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -94,6 +94,13 @@ async function loginUserController(req, res) {
     })
 }
 
+
+/**
+ * 
+ * @name logoutUserController 
+ * @description clear token from user cookie and add the token in the blacklist
+ * @access Public
+ */
 async function logoutUserController(req, res) {
     const token = req.cookies.token
     
@@ -107,9 +114,28 @@ async function logoutUserController(req, res) {
         message: "User logged out successfully"
     })
 }
+/**
+ * @name getMeController
+ * @description get the current logged in user details.
+ * @access  Private
+ */
+async function getMeController(req, res) {
+    
+    const user = await userModel.findById(req.user.id)
+
+    res.status(200).json({
+        message: "User details fetched successfully",
+        user:{
+            id: user._id,
+            username: user.username,
+            email: user.email
+        }
+    })
+}
 
 module.exports = {
     registerUserController,
     loginUserController,
-    logoutUserController
+    logoutUserController,
+    getMeController
 }
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,6 @@
 const { Router } = require('express')
 const authController = require("../controllers/auth.controller")
-
+const authMiddleware = require("../middlewares/auth.middleware")
 const authRouter = Router()
 
 /**
@@ -27,6 +27,11 @@ authRouter.post("/login",authController.loginUserController)
 authRouter.get("/logout",authController.logoutUserController)
 
 
-
+/**
+ * @route GET /api/auth/get-me
+ * @description get the current logged in user details
+ * @access Private
+ */
+authRouter.get("/get-me", authMiddleware.authUser, authController.getMeController)
 
 module.exports = authRouter
