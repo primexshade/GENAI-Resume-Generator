bceb9238ece047ca812fe00f1077e3ca2105d8c5
Aryan Tiwari <taryan54@gmail.com>
2026-05-25 21:52:22 +0530

    Authnitaction Form Styling and Frontend Architecture

--- frontend/src/features/auth/pages/Login.jsx
+++ frontend/src/features/auth/pages/Login.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "../auth.form.scss"
+import { useNavigate, Link } from 'react-router'
 
 const Login = () => {
     
@@ -29,6 +30,8 @@ const Login = () => {
                     <button className='button primary-button'>Login</button>
 
                 </form>
+
+                <p>Create an account <Link to={"/register"}>Register</Link></p>
             </div>
         </main>
     )
